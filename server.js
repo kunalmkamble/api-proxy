38d6d9d34876cb6353dfe88b996d4b26ca700abe
Kunal Kamble <kunal.kamble@calsoftinc.com>
2024-01-03 10:56:52 +0530

    [update] moved to https

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const httpProxy = require('http-proxy');
 
 const app = express();
@@ -11,10 +13,15 @@ app.use((req, res, next) => {
 });
 
 // Create a generic proxy server
-const proxy = httpProxy.createProxyServer({ target: 'http://localhost:1337', changeOrigin: true });
+const proxy = httpProxy.createProxyServer({
+    target: 'http://localhost:1337',
+    changeOrigin: true,
+    secure: false, // This line may help, but it's not recommended for production
+    protocol: 'https:',
+});
 
 // Handle proxy requests
-app.use('/', (req, res) => {
+app.use('/proxy', (req, res) => {
     proxy.web(req, res);
 });
 
@@ -24,6 +31,13 @@ proxy.on('error', (err, req, res) => {
     res.status(500).send('Proxy Error');
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+// Set up HTTPS server
+const privateKey = fs.readFileSync('private-key.pem', 'utf8');
+const certificate = fs.readFileSync('public-cert.pem', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port, () => {
+    console.log(`Server is running on https://localhost:${port}`);
 });
